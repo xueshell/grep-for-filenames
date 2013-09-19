e357042dc74705f81ed57a36bc06f6d123c97fab
luka <luka.zitnik@gmail.com>
2013-09-19 22:48:45 +0200

    added a few more exploratory tests

--- tests/exploratoryTest.sh
+++ tests/exploratoryTest.sh
@@ -25,4 +25,30 @@ function testCanBrowseToTheMain() {
     assertEquals $expected $actual
 }
 
+function testMultipleLinesString() {
+    str='a
+b
+c'
+    assertEquals $'a\nb\nc' "$str"
+}
+
+function testMultipleLinesInDoubleQuotes() {
+    var1=1
+    var2=2
+    var3=3
+    str="$var1
+$var2
+$var3"
+    assertEquals $'1\n2\n3' "$str"
+}
+
+function testHeredoc() {
+    read -d '' str<<-____LINES
+	a
+	b
+	c
+____LINES
+    assertEquals $'a\nb\nc' "$str"
+}
+
 . ./shunit2-2.1.6/src/shunit2
